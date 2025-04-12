967432b9f9d7da9880e0ec0e43aafef546376f34
Josue Morales <dev.carlos.morales@gmail.com>
2025-04-11 19:08:08 -0500

    Add DateTimePicker component with date and time selection functionality

---
+++ src/app/components/DatePicker.tsx
@@ -0,0 +1,152 @@
+'use client';
+
+import { format } from 'date-fns';
+
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { FormControl } from '@/components/ui/form';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { cn } from '@/lib/utils';
+import { CalendarIcon } from 'lucide-react';
+import { es } from 'date-fns/locale';
+
+interface DateTimePickerProps {
+  onDateChange: (date: Date | undefined) => void;
+  selectedDate: Date | undefined;
+}
+
+export function DateTimePickerForm({
+  onDateChange,
+  selectedDate,
+}: DateTimePickerProps) {
+  function handleDateSelect(newDate: Date | undefined) {
+    onDateChange(newDate);
+  }
+
+  function handleTimeChange(type: 'hour' | 'minute' | 'ampm', value: string) {
+    const currentDate = selectedDate || new Date();
+    let newDate = new Date(currentDate);
+
+    if (type === 'hour') {
+      const hour = parseInt(value, 10);
+      newDate.setHours(newDate.getHours() >= 12 ? hour + 12 : hour);
+    } else if (type === 'minute') {
+      newDate.setMinutes(parseInt(value, 10));
+    } else if (type === 'ampm') {
+      const hours = newDate.getHours();
+      if (value === 'AM' && hours >= 12) {
+        newDate.setHours(hours - 12);
+      } else if (value === 'PM' && hours < 12) {
+        newDate.setHours(hours + 12);
+      }
+    }
+
+    onDateChange(newDate);
+  }
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <FormControl>
+          <Button
+            variant={'outline'}
+            className={cn(
+              'w-full pl-3 text-left font-normal',
+              !selectedDate && 'text-muted-foreground'
+            )}
+          >
+            {selectedDate ? (
+              format(selectedDate, 'dd/MM/yyyy hh:mm aa')
+            ) : (
+              <span>MM/DD/YYYY hh:mm aa</span>
+            )}
+            <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+          </Button>
+        </FormControl>
+      </PopoverTrigger>
+      <PopoverContent className='w-auto p-0'>
+        <div className='sm:flex'>
+          <Calendar
+            mode='single'
+            selected={selectedDate}
+            onSelect={handleDateSelect}
+            locale={es}
+          />
+          <div className='flex flex-col sm:flex-row sm:h-[300px] divide-y sm:divide-y-0 sm:divide-x'>
+            <ScrollArea className='w-64 sm:w-auto'>
+              <div className='flex sm:flex-col p-2'>
+                {Array.from({ length: 12 }, (_, i) => i + 1)
+                  .reverse()
+                  .map((hour) => (
+                    <Button
+                      key={hour}
+                      size='icon'
+                      variant={
+                        selectedDate &&
+                        selectedDate.getHours() % 12 === hour % 12
+                          ? 'default'
+                          : 'ghost'
+                      }
+                      className='sm:w-full shrink-0 aspect-square'
+                      onClick={() => handleTimeChange('hour', hour.toString())}
+                    >
+                      {hour}
+                    </Button>
+                  ))}
+              </div>
+              <ScrollBar orientation='horizontal' className='sm:hidden' />
+            </ScrollArea>
+            <ScrollArea className='w-64 sm:w-auto'>
+              <div className='flex sm:flex-col p-2'>
+                {Array.from({ length: 12 }, (_, i) => i * 5).map((minute) => (
+                  <Button
+                    key={minute}
+                    size='icon'
+                    variant={
+                      selectedDate && selectedDate.getMinutes() === minute
+                        ? 'default'
+                        : 'ghost'
+                    }
+                    className='sm:w-full shrink-0 aspect-square'
+                    onClick={() =>
+                      handleTimeChange('minute', minute.toString())
+                    }
+                  >
+                    {minute.toString().padStart(2, '0')}
+                  </Button>
+                ))}
+              </div>
+              <ScrollBar orientation='horizontal' className='sm:hidden' />
+            </ScrollArea>
+            <ScrollArea className=''>
+              <div className='flex sm:flex-col p-2'>
+                {['AM', 'PM'].map((ampm) => (
+                  <Button
+                    key={ampm}
+                    size='icon'
+                    variant={
+                      selectedDate &&
+                      ((ampm === 'AM' && selectedDate.getHours() < 12) ||
+                        (ampm === 'PM' && selectedDate.getHours() >= 12))
+                        ? 'default'
+                        : 'ghost'
+                    }
+                    className='sm:w-full shrink-0 aspect-square'
+                    onClick={() => handleTimeChange('ampm', ampm)}
+                  >
+                    {ampm}
+                  </Button>
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
